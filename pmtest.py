6f3e9adddf4b79ca682a2f7fd7815b42c9759253
leeshinhyeong <san2996@daum.net>
2019-09-03 22:13:30 +0900

    pmtest7

--- pmtest.py
+++ pmtest.py
@@ -72,7 +72,7 @@ class SDS021Reader:
 
 
 
-                sql = """insert into dust(drone_id, dust_id, chkpmValue, pm25Value, pm10Value, datecreated) values('drone01', 'dust01', 'Eulgiro4ga', %s, %s, now())""";
+                sql = """insert into dust_drone(drone_id, dust_id, chkpmValue, pm25Value, pm10Value, datecreated) values('drone01', 'dust01', 'Eulgiro4ga', %s, %s, now())""";
 
                 curs.execute(sql, (values[0], values[0]))
 
