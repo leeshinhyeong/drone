202bf0180f8a3ca0bd1fec6317a3efe49e6cfc08
leeshinhyeong <san2996@daum.net>
2019-09-03 21:06:20 +0900

    mysql test

---
+++ insert.py
@@ -0,0 +1,13 @@
+def db_insert(a, b, c) :
+    # DB Connect
+    conn = pymysql.connect(host='52.231.75.145', user='root', password='1234',db='mysql', charset='utf8')
+    curs = conn.cursor()
+
+    sql = """insert into dust_airkorea(gps_id, pm10Value, pm25Value, datecreated) values(%s, %s, %s, now())""";
+    curs.execute(sql, (a, b, c))
+    conn.commit()
+    conn.close()
+    #@sys.exit(1)
+
+db_insert(12321321,12,35)
+
---
+++ insert2.py
@@ -0,0 +1,9 @@
+# DB Connect
+conn = pymysql.connect(host='52.231.75.145', user='root', password='1234',db='mysql', charset='utf8')
+curs = conn.cursor()
+sql = """insert into dust_airkorea(gps_id, pm10Value, pm25Value, datecreated) values(%s, %s, %s, now())""";
+curs.execute(sql, ('123', '123', '123'))
+conn.commit()
+conn.close()
+#@sys.exit(1)
+
